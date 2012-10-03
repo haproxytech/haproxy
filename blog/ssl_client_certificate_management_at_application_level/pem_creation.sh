4bf82c64d3e9a5342ed67fce563f01b9585ad5cd
bedis <bedis9@gmail.com>
2012-10-03 10:18:44 +0200

    comments added + ca.pem creation

--- blog/ssl_client_certificate_management_at_application_level/pem_creation.sh
+++ blog/ssl_client_certificate_management_at_application_level/pem_creation.sh
@@ -1,3 +1,8 @@
+# server PEM file creation (order matters)
 cat server.crt >  server.pem
 cat server.key >> server.pem
+
+# certificate authority concatenation (order doesn't matters)
+cat ca.crt > ca.pem
+cat ca2.crt >> ca.pem
 
