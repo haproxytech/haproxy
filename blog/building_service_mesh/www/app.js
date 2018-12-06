b5a6c024a5210c724d0ffe5e0ddde8f874e4ed90
Nick Ramirez <nramirez@haproxy.com>
2018-12-06 09:01:11 -0500

    Adding docker-compose, Docker files, web and redis projects to Consul/HAProxy example.
    Fixed:
    
    * Removed "if type(serial) == 'nil'" in authorize.lua because serial was always nil.
    * Removed "create = core.tcp" in authorize.lua because it seemed to cause the HTTP request to silently fail.
    * Added second condition to "if [ "${CERTS[$cert]}" != "$SERIAL" ] || [ ! -s "${CONF_PROD_DIR}/${CONF_CERTS_DIRNAME}/${cert}.pem" ]" because otherwise, the file may get created but be empty and the program doesn't recover.
    * Changed how upstreams port is got -- now getting from /catalog/service/:service, filtering to .ServicePort, instead of using local service port (which is different if Consul autoassigns the port)
    * Removed ${CONFIG["BIND_ADDRESS"]} in places where it would set the address to a non-localhost IP, but Consul tries to healthcheck localhost.
    
    Reverting timeouts because they aren't needed.

---
+++ blog/building_service_mesh/www/app.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const redis = require('redis')
+const app = express()
+const port = 8080
+const redisUrl = process.env.REDIS_URL
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/', function(req, res, next) {
+  var redisClient = null
+
+    var message = ""
+    redisClient = redis.createClient({
+      host: redisUrl,
+      retry_strategy: function(args) {
+        return undefined
+      }
+    })
+
+    redisClient.on("error", function(err) {
+      console.log("Error: " + err)
+      message = err
+      res.send(`
+          <h1>App served by Consul and HAProxy!</h1>
+          <form action="/" method="post">
+            <label for="message">Add message</label>
+            <input type="text" name="message" />
+            <input type="submit" value="Save to Redis" />
+          </form>
+          <p>Message: ${message}</p>`)
+      next()
+    })
+
+    redisClient.get('message', function(err, reply) {
+      if (err != null) {
+        console.log("Error: " + err)
+      }
+      else {
+        message = reply
+        res.send(`
+          <h1>App served by Consul and HAProxy!</h1>
+          <form action="/" method="post">
+            <label for="message">Add message</label>
+            <input type="text" name="message" />
+            <input type="submit" value="Save to Redis" />
+          </form>
+          <p>Message: ${message}</p>`)
+      }
+    })
+
+    redisClient.quit()
+})
+
+app.post('/', function(req, res, next) {
+  var redisClient = null
+  var message = req.body.message
+
+  redisClient = redis.createClient({
+    host: redisUrl,
+    retry_strategy: function(args) {
+      return undefined
+    }
+  })
+
+  redisClient.on("error", function(err) {
+    console.log("Error: " + err)
+    next()
+  })
+
+  redisClient.set('message', message)
+  redisClient.quit()
+  res.redirect(303, '/')
+})
+
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`)
+})
